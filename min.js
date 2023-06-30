bdc31896a184e4fb57b6b2b521cb6bdec6d8c124
prachi singh <prachisingh7921@gmail.com>
2023-06-30 11:37:04 +0530

    first commit

---
+++ min.js
@@ -0,0 +1,8 @@
+// Write a JavaScript program to find the length of a string.
+var a;
+a="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+document.write(a.length);
+
+var b;
+b="Sonu Bhaiya";
+// document.write(b.length);
